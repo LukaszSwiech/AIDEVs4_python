6ffd8fe0729c8a548361cc95002165a1c6a6cd42
LukaszSwiech <ls.aidev@proton.me>
2026-08-17 11:22:22 +0200

    feat(s01e04): wire app entrypoint to init MCP, run agent, log result, cleanup.

--- s01e04/app.py
+++ s01e04/app.py
@@ -1,10 +1,11 @@
 import logging
 import asyncio
-
 from .mcp_agent import client as mcp_client
 
+from .agent import run_proxy_agent
 from ..common.logs import setup_logging
 from ..common.agent import make_mcp_tool_executor
+from ..common.ai import token_usage
 
 async def init_mcp() -> mcp_client.MCPClient:
     client = mcp_client.MCPClient()
@@ -12,14 +13,22 @@ async def init_mcp() -> mcp_client.MCPClient:
     return client
 
 async def main():
+
     setup_logging()
 
     client = await init_mcp()
 
-    execute_tool =  make_mcp_tool_executor(client.server_name, client.session.call_tool)
-    tools = client.open_ai_tools
+    try:
+        execute_tool =  make_mcp_tool_executor(client.server_name, client.session.call_tool)
+        tools = client.open_ai_tools
 
-    
+        solve_exercise = await run_proxy_agent(tools, execute_tool)
+        logging.info(f"Agent zakonczyl zadanie. Jego odpowiedz to {solve_exercise}")
+
+    finally:
+        logging.info("Shutting down MCP server...")
+        await client.cleanup()
+        token_usage.log_total()
 
 if __name__ == "__main__":
     asyncio.run(main())
